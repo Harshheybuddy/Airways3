dbc24214ede0ac6f7e86fb61c274fe401d067fb9
Harshheybuddy <164989804+Harshheybuddy@users.noreply.github.com>
2024-09-27 00:06:31 +0530

    Update Section1.jsx

--- src/pages/home/section1/Section1.jsx
+++ src/pages/home/section1/Section1.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from "./styles.module.css"
 
 import Searchbar from '../searchbar/Searchbar'
+import { useLocation } from 'react-router-dom';
 
 const Section1 = () => {
+
+  const location = useLocation();
+  const offset=1000;
+  useEffect(() => {
+    if (location.hash) {
+      const section = document.querySelector(location.hash);
+      if (section) {
+        const sectionTop = section.getBoundingClientRect().top;
+
+        // Smooth scroll to the section with the offset
+        window.scrollTo({
+          top: sectionTop-1000,
+          behavior: 'smooth'
+        });
+      }
+    }
+  }, [location, offset]);
   return (
-    <div className={`${styles.container} relative flex flex-col gap-[3rem] items-center`}>
+    <div  className={`${styles.container} relative flex flex-col gap-[3rem] items-center`}>
       <div className='flex flex-col gap-[0rem] items-center'>
         <h1 className={styles.heading}>Charter Your Private Jet Today</h1>
         <p className={styles.para}>Revolutionary and Sustainable Private Jet Charter: Pioneering Excellence in Aviation</p>
@@ -18,7 +36,7 @@ const Section1 = () => {
         </div>
       </div>
       
-      <div className={styles.searchbarWrapper}>
+      <div id="bookNow" className={styles.searchbarWrapper}>
         <Searchbar/>
       </div>
     </div>
